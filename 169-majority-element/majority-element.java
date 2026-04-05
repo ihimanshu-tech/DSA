655cbb3a668a278f398ab4c9fe1f3d9137a4a470
Himanshu <workathiman@gmail.com>
2026-04-05 17:50:23 +0530

    Time: 1 ms (99.84%) | Memory: 55.8 MB (40.29%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,20 +1,22 @@
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = 0;
-        int count = 0;
 
-        for (int num : nums) {
-            if (count == 0) {
-                candidate = num;
+        int ele = 0;
+        int cnt = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(cnt ==0){
+                // cnt = 1; // if count will be equal to 1 and last pairing off will be canceled and answer can be wrong 
+                ele = nums[i];
             }
-
-            if (num == candidate) {
-                count++;
-            } else {
-                count--;
+            if(nums[i] == ele){
+                cnt++;
+            }
+            else {
+                cnt--;
             }
         }
-
-        return candidate;
+        return ele;
+     
     }
 }
