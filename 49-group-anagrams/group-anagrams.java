219f9a05a4fc3564c1044ef8b8e82af74ee746e1
Himanshu <workathiman@gmail.com>
2026-04-11 11:36:12 +0530

    Time: 18 ms (24.24%) | Memory: 50.8 MB (11.66%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            // Count frequency of each character
+            int[] count = new int[26];
+            
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            
+            // Convert count array to string key
+            String key = Arrays.toString(count);
+            
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
