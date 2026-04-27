4c09100c5579e331ce362e7efa5745de91e56233
Himanshu <workathiman@gmail.com>
2026-04-27 22:19:15 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (93.77%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -1,20 +1,9 @@
 class Solution {
     public void reverseString(char[] s) {
-        helper(s, 0, s.length - 1);
-    }
-
-    private void helper(char[] s, int left, int right) {
-        // Base case: when pointers meet or cross
-        if (left >= right) {
-            return;
+        for (int i = 0; i < s.length / 2; i++) {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
         }
-
-        // Swap characters at the current pointers
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
-
-        // Recursive call moving pointers inward
-        helper(s, left + 1, right - 1);
     }
 }
