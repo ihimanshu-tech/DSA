c8b4963efd102b4382d7b4b076027c5abdbb53b7
Himanshu <workathiman@gmail.com>
2026-04-28 18:18:11 +0530

    Time: 2 ms (99.17%) | Memory: 42 MB (77.20%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,21 +1,38 @@
 class Solution {
     int result = 0;
+
     public int countArrangement(int n) {
-        int[] nums = new int[n+1];
-        dfs(nums,1,n);
+        int[] nums = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            nums[i] = i; // Fix: Assign the values
+        }
+        dfs(nums, n); // Start from n and go down to 1
         return result;
     }
-    void dfs (int[] nums,int val,int n){
-        if(val > n){
+
+    void dfs(int[] nums, int val) {
+        // Base case: if we've successfully placed all numbers down to 1
+        if (val == 0) {
             result++;
             return;
         }
-        for(int i =1; i<=n; i++){
-            if(nums[i] == 0 && (val%i==0 || i%val==0)){
-                nums[i] = val;
-                dfs(nums,val+1,n);
-                nums[i] = 0;
+
+        for (int i = val; i > 0; i--) {
+            swap(nums, i, val);
+            
+            // Check if the number we just put at index 'val' is "beautiful"
+            // The condition: (number % index == 0) OR (index % number == 0)
+            if (nums[val] % val == 0 || val % nums[val] == 0) {
+                dfs(nums, val - 1);
             }
+            
+            swap(nums, i, val); // Backtrack
         }
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int tmp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = tmp;
     }
 }
