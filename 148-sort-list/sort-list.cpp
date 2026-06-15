566a3d5b86aa393d1f466efb9d3bef240b383e87
Himanshu <workathiman@gmail.com>
2026-06-15 09:54:02 +0530

    Time: 16 ms (59.26%) | Memory: 60.3 MB (49.38%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,5 +1,9 @@
 class Solution {
+    static bool solve(ListNode* a,ListNode* b){
+        return a->val<b->val;
+    }
 public:
+//method 2 - inbuilt sort function
     ListNode* sortList(ListNode* head) {
         if (!head || !head->next) 
             return head;
@@ -9,48 +13,12 @@ public:
             arr.push_back(head);
             head = head->next;
         }
-        mergeSort(arr,0,arr.size()-1);
+        // mergeSort(arr,0,arr.size()-1);
+        sort(arr.begin(),arr.end(),solve);
         for(int i =0;i<arr.size()-1;i++){
             arr[i]->next = arr[i+1];
         }
         arr[arr.size()-1]->next =nullptr;
         return arr[0];
-    }
-
-private:
-    void mergeSort(vector<ListNode*>& arr,int s,int e) {
-            if(s>=e){
-                return;
-            }
-            int mid = s +(e-s)/2;
-            mergeSort(arr,s,mid);
-            mergeSort(arr,mid+1,e);
-            merge(arr,s,e);
-        }
-    void merge(vector<ListNode*>& arr,int s,int e){
-        vector<ListNode*> mix;
-        int mid = s+(e-s)/2;
-        int i= s ,j = mid+1;
-        while(i<=mid && j<=e){
-            if(arr[i]->val<=arr[j]->val){
-                mix.push_back(arr[i]);
-                i++;
-            }
-            else{
-                mix.push_back(arr[j]);
-                j++;
-            }   
-        }
-        while(i<=mid){
-            mix.push_back(arr[i]);
-            i++;            
-        } 
-        while(j<=e){
-            mix.push_back(arr[j]);
-            j++;            
-        } 
-        for(int i=s,j=0;i<=e;i++,j++){
-            arr[i] = mix[j];
-        }
     }
 };
