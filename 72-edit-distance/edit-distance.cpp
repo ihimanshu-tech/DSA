d3ebd9e5a305bf1756cc660437a9d20f4bc71b67
Himanshu <workathiman@gmail.com>
2026-07-11 12:15:58 +0530

    Time: 2 ms (93.77%) | Memory: 13.3 MB (30.26%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,28 +1,28 @@
 class Solution {
-    int solve(string a, string b,int i,int j, vector<vector<int>> &dp){
-        if(i==a.length())
-            return b.length() -j;
-        if(j==b.length())
-            return a.length() -i;
+    int solve(const string &a, const string &b, int i, int j, vector<vector<int>>& dp) {
+        if (i == a.length())
+            return b.length() - j;
+        if (j == b.length())
+            return a.length() - i;
 
-        if(dp[i][j] !=-1)
+        if (dp[i][j] != -1)
             return dp[i][j];
 
-    int ans = 0;
-    if(a[i]==b[j]) return solve(a,b,i+1,j+1,dp);
+        if (a[i] == b[j])
+            return dp[i][j] = solve(a, b, i + 1, j + 1, dp);
 
-    else{
-        int insertAns = 1+solve(a,b,i,j+1,dp);
-        int deleteAns = 1+solve(a,b,i+1,j,dp);
-        int replaceAns = 1+solve(a,b,i+1,j+1,dp);
+        else {
+            int insertAns = 1 + solve(a, b, i, j + 1, dp);
+            int deleteAns = 1 + solve(a, b, i + 1, j, dp);
+            int replaceAns = 1 + solve(a, b, i + 1, j + 1, dp);
 
-        ans += min(replaceAns,min(insertAns,deleteAns));
+            return dp[i][j] = min(replaceAns, min(insertAns, deleteAns));
+        }
     }
-    return dp[i][j] = ans;
-    }
+
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.length(), vector<int>(word2.length(), -1));
-        return solve(word1,word2,0,0,dp);
+        vector<vector<int>> dp(word1.length() +1, vector<int>(word2.length() + 1 , -1));
+        return solve(word1, word2, 0, 0, dp);
     }
 };
