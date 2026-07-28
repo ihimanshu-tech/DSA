980124de411e3eccc61b273902c48401455c629e
Himanshu <workathiman@gmail.com>
2026-07-28 14:57:50 +0530

    Time: 8 ms (51.09%) | Memory: 46.3 MB (36.20%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,45 +1,20 @@
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    class TrieNode {
-        TrieNode[] children = new TrieNode[26];
-        boolean isEndOfWord = false;
-
-        public void insert(String word) {
-            TrieNode curr = this;
-            for (char c : word.toCharArray()) {
-                int idx = c - 'a';
-                if (curr.children[idx] == null) {
-                    curr.children[idx] = new TrieNode();
-                }
-                curr = curr.children[idx];
-            }
-            curr.isEndOfWord = true;
-        }
-    }
-
     public boolean wordBreak(String s, List<String> wordDict) {
-        TrieNode root = new TrieNode();
-        for (String word : wordDict) {
-            root.insert(word);
-        }
-
+        Set<String> wordSet = new HashSet<>(wordDict);
         int n = s.length();
+        
+        
         boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
+        dp[0] = true; 
 
-        for (int i = 0; i < n; i++) {
-            if (!dp[i]) continue;
-
-            TrieNode curr = root;
-            for (int j = i; j < n; j++) {
-                int idx = s.charAt(j) - 'a';
-                if (curr.children[idx] == null) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                
+                if (dp[j] && wordSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
                     break; 
-                }
-                curr = curr.children[idx];
-                if (curr.isEndOfWord) {
-                    dp[j + 1] = true;
                 }
             }
         }
