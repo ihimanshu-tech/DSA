afa52879f201cd3b6c3adaab069dab5a74359d43
Himanshu <workathiman@gmail.com>
2026-09-17 07:34:25 +0530

    Time: 1 ms (100.00%) | Memory: 48.3 MB (8.65%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -4,16 +4,19 @@ class Solution {
         for(int val : nums){
             xorr^=val;
         }
-        xorr &= -xorr;
-        // long int rightmost = (xorr & xorr-1)^xorr;
+        int mask = xorr & -xorr;
+        
         int[] res ={0,0};
         for(int val : nums){
-            if((val & xorr) == 0)
-                res[0] ^= val;
+            if((val & mask) == 0)
+                res[0] ^= val; //group A
             else
-                res[1] ^= val;
+                res[1] ^= val; //group B
         }
-
         return res;
     }
 }
+// xorr will give xorr of that two single number
+// mask of 2's complement will give rightmost set bit(1)
+// group A will contain a single number along with duplicate numbers
+// group B will contain another single number along with different duplicate numbers
