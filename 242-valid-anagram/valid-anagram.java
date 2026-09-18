64fe0cd272420e01863589987ae42e11d9b4abec
Himanshu <workathiman@gmail.com>
2026-09-18 22:20:31 +0530

    Time: 6 ms (44.11%) | Memory: 46.5 MB (24.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,14 @@
 class Solution {
-    public boolean isAnagram(String a, String b) {
-        char[] arr1 = a.toCharArray();
-        char[] arr2 = b.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        if (arr1.length == arr2.length) {
-            for (int i = 0; i < arr1.length; i++) {
-                    if (arr1[i] != arr2[i]){
-                        return false;
-                    }
-                }
-            return true;
-            }
-        return false;
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] arr_s = s.toCharArray();
+        char[] arr_t = t.toCharArray();
+        Arrays.sort(arr_s);
+        Arrays.sort(arr_t);
+        for(int i=0;i<arr_s.length;i++){
+            if(arr_s[i] != arr_t[i])
+                return false;
+        }
+        return true;
     }
 }
